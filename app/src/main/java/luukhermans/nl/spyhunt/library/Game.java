3d67456dc9f4b095b78f36f3c85d0977743c2a81
bras1223 <l.hermans98@gmail.com>
2017-02-07 11:52:13 +0100

    commit

--- app/src/main/java/luukhermans/nl/spyhunt/library/Game.java
+++ app/src/main/java/luukhermans/nl/spyhunt/library/Game.java
@@ -1,8 +1,14 @@
 package luukhermans.nl.spyhunt.library;
 
+import java.util.List;
+
 /**
  * Created by Luuk on 7-2-2017.
  */
 
 public class Game {
+
+    private Player currentPlayer;
+    private List<Player> playersList;
+
 }
